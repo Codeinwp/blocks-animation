4ae5ac0c89eef99406e22eb541b3480f9cbea97c
Hardeep Asrani <hardeepasrani@gmail.com>
2019-04-30 13:04:20 +0530

    Update blocks-animation.php

--- blocks-animation.php
+++ blocks-animation.php
@@ -8,7 +8,7 @@
  * Author URI:        https://themeisle.com
  * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
- * Text Domain:       blocks-css
+ * Text Domain:       blocks-animation
  * Domain Path:       /languages
  * WordPress Available:  yes
  * Requires License:    no
